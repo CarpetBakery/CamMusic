f132e2f8e1a0d8fae7f9c85bf01b1440f3114508
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2025-07-12 20:34:47 -0400

    add test midi

--- src/test/MeltyTest.cs
+++ src/test/MeltyTest.cs
@@ -1,7 +1,12 @@
 using Godot;
 using System;
+using MeltySynth;
 
 public partial class MeltyTest : Node
 {
-	
+    public override void _Ready()
+    {
+        base._Ready();
+        GD.Print("Testing Meltysynth...");
+    }
 }
