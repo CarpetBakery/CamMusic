29dd03204a1026659a5065f2b26f2dbe457ce89d
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2025-07-29 13:43:15 -0400

    more docs

--- CamMusic/src/kodot2.gd
+++ CamMusic/src/kodot2.gd
@@ -34,7 +34,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	update(delta)
 	
-	var joints := getBodyJointPositions2D(-1)
+	var joints := getBodyJointPositions2D()
 	if joints.is_empty():
 		return
 	
