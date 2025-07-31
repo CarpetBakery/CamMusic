87dd71ae21f597c3946fcc64e68c85de656e5ea9
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2025-07-31 14:14:29 -0400

    just use godot environment var

--- gendocs.bat
+++ gendocs.bat
@@ -1,4 +1,4 @@
 @echo off
 
 cd CamMusic
-"../../../Engine/Godot_v4.4.1-DEBUG_mono/godot.windows.editor.x86_64.mono.console.exe" --doctool ../ --gdextension-docs
+godot --doctool ../ --gdextension-docs
